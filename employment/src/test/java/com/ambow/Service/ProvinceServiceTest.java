028fd1b425a2ce77940a4b2052fc9cd8707b7701
hqz <86598342@qq.com>
2021-09-24 09:32:56 +0800

    employment测试1

--- employment/src/test/java/com/ambow/Service/ProvinceServiceTest.java
+++ employment/src/test/java/com/ambow/Service/ProvinceServiceTest.java
@@ -2,12 +2,9 @@ package com.ambow.Service;
 
 import com.ambow.dao.ProvinceDao;
 import com.ambow.entity.Province;
-import com.ambow.service.ProvinceService;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
@@ -17,7 +14,6 @@ public class ProvinceServiceTest {
     @Test
     public void selectProvinceTest(){
         ApplicationContext app = new ClassPathXmlApplicationContext("spring.xml");
-        //rovinceDao provinceDao = (ProvinceDao) app.getBean("provinceDao");
         ProvinceDao provinceDao = app.getBean(ProvinceDao.class);
         List<Province> list = provinceDao.selectProvinceAll();
         System.out.println(list);
