1d849ba9c8dd0850b370019ff4da42b3f0eedf1a
miao <2216665029@qq.com>
2021-09-27 14:07:32 +0800

    省份添加和删除

--- employment/src/main/java/com/ambow/controller/CityController.java
+++ employment/src/main/java/com/ambow/controller/CityController.java
@@ -1,12 +1,16 @@
 package com.ambow.controller;
 
 import com.ambow.entity.City;
+import com.ambow.entity.Province;
 import com.ambow.service.CityService;
+import com.ambow.service.ProvinceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -15,6 +19,8 @@ public class CityController {
 
     @Resource
     private CityService cityService;
+    @Resource
+    private ProvinceService provinceService;
 
     @RequestMapping("selectCityAll")
     public String selectCityAll(Model model){
@@ -22,4 +28,43 @@ public class CityController {
         model.addAttribute("cityList",cityList);
         return "city_list";
     }
+    @RequestMapping("addCity")
+    @ResponseBody
+    public String addCity(City city){
+        int res = cityService.addCity(city);
+        if(res>0){
+            return "true";
+        }return "false";
+    }
+    @RequestMapping("updateCity")
+    @ResponseBody
+    public String updateCity(City city){
+        int res=cityService.updateCity(city);
+        if(res>0){
+            return "true";
+        }return "false";
+    }
+    @RequestMapping("delCity")
+    @ResponseBody
+    public String delCity(int cid){
+        int res = cityService.delCity(cid);
+        if(res>0){
+            return "true";
+        }return "false";
+    }
+
+    @RequestMapping("selectCityById")
+    @ResponseBody
+    public String selectCityById(int cid, HttpServletRequest request){
+        City city = cityService.selectCityById(cid);
+        request.setAttribute("city",city);
+        return " city_edit";
+    }
+    @RequestMapping("selectProvinceAll")
+    public String selectProvinceAll(Model model){
+        List<Province> provinceList = provinceService.selectProvinceAll();
+        model.addAttribute("provinceList",provinceList);
+        return "city_add";
+    }
+
 }
--- employment/src/main/webapp/city_list.jsp
+++ employment/src/main/webapp/city_list.jsp
@@ -61,7 +61,7 @@
 
                     <div class="layui-card-header">
                         <%--<button class="layui-btn layui-btn-danger" onclick="delAll()"><i class="layui-icon"></i>批量删除</button>--%>
-                        <button class="layui-btn" onclick="xadmin.open('添加城市','${pageContext.request.contextPath}/province_add.jsp',500,300)"><i class="layui-icon"></i>添加</button>
+                        <button class="layui-btn" onclick="xadmin.open('添加城市','${pageContext.request.contextPath}/city/selectProvinceAll',500,300)"><i class="layui-icon"></i>添加</button>
                     </div>
                 <div class="layui-card-body layui-table-body layui-table-main">
                     <table class="layui-table layui-form">
