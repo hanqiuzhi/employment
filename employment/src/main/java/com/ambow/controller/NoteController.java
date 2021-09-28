f52138575d4299787c4d64b5d0d938501fe87007
pjl <pjl19991014@163.com>
2021-09-28 11:26:09 +0800

    招聘记录表的查,修改,删除的套模板第一次提交

--- employment/src/main/java/com/ambow/controller/NoteController.java
+++ employment/src/main/java/com/ambow/controller/NoteController.java
@@ -1,14 +1,19 @@
 package com.ambow.controller;
 
 import com.ambow.entity.City;
+import com.ambow.entity.Hiring;
 import com.ambow.entity.Note;
+import com.ambow.entity.Student;
 import com.ambow.service.CityService;
 import com.ambow.service.NoteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -24,4 +29,47 @@ public class NoteController {
         model.addAttribute("noteList",noteList);
         return "note_list";
     }
+
+    //学生点击招聘信息，点击提交申请面试，招聘记录便自动生成
+    @RequestMapping("noteAdd")
+    @ResponseBody
+    public String noteAdd(Note note, HttpSession session,Model model){
+
+        Student student=(Student) session.getAttribute("student");
+        student.getSid();
+        note.setNsid(student);
+        Hiring hiring=(Hiring)session.getAttribute("hiring");
+        hiring.getHid();
+        note.setNhid(hiring);
+        note.setNflag(0);
+        noteService.addNote(note);
+        selectNoteAll(model);
+        return "note_list";
+    }
+
+    @RequestMapping("noteById")
+    public String noteById(int nid,Model model){
+        Note note=noteService.selectNoteById(nid);
+        model.addAttribute("note1",note);
+        return "note_edit";
+    }
+
+    @RequestMapping("noteUpdate")
+    @ResponseBody
+    public String noteUpdate(Note note){
+        int res=noteService.updateNote(note);
+        if(res>0){
+            return "true";
+        }return "false";
+    }
+
+    @RequestMapping("noteDel")
+    @ResponseBody
+    public String noteDel(int nid){
+        int res=noteService.delNote(nid);
+        if(res>0){
+            return "true";
+        }return "false";
+    }
+
 }
