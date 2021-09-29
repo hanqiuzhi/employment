420ca7b172d9b6dedf5dea956bd5c7399e0f278a
lzx <1586318059@qq.com>
2021-09-29 09:43:30 +0800

    学校密码修改

--- employment/src/main/java/com/ambow/controller/UniversityController.java
+++ employment/src/main/java/com/ambow/controller/UniversityController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -22,6 +24,22 @@ public class UniversityController {
         List<University> universityList = universityService.selectUniversityAll();
         model.addAttribute("universityList",universityList);
         return "university_list";
+    }
+
+    @RequestMapping("updateUniversity")
+    @ResponseBody
+    public String updateUniversity(University university){
+        int res = universityService.updateUniversity(university);
+        if(res>0){
+            return "true";
+        }return "false";
+    }
+
+    @RequestMapping("selectUniversityById")
+    public String selectProvinceById(int uid, HttpServletRequest request ){
+        University university = universityService.selectUniversityById(uid);
+        request.setAttribute("university",university);
+        return "university_edit";
     }
 
 }
